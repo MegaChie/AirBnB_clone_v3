9551cd071f8c709cbee6b655bbce4ae19d9a576d
MegaChie <workspace.mega@gmail.com>
2024-03-29 17:07:13 -0700

    Output identical to project's

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -10,3 +10,11 @@ def getStat():
     """returns the status of the API if working"""
     goodStat = {"status": "OK"}
     return json.dumps(goodStat, indent=2), {"Content-Type": "application/json"}
+
+
+
+@app_views.after_request
+def add_newline(response):
+    if response.is_json:
+        response.data += b'\n'
+    return response
