9d2ecb1446aea25eb23e78174b66a1f278f4435b
MegaChie <workspace.mega@gmail.com>
2024-03-30 13:49:27 -0700

    HTTP DELETE method for the State objects

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -11,13 +11,15 @@ from models import storage
 
 
 @app_views.route("/states", methods=["GET"], strict_slashes=False)
-@app_views.route("/states/<state_id>", methods=["GET"],
+@app_views.route("/states/<state_id>", methods=["GET", "DELETE"],
                  strict_slashes=False)
 def stateEdit(state_id=None):
     """
     Edit the State objects according to the specified HTTP method:
       - GET: Retrieves the list of all State objects.
-             Or one object if ID is passed
+             Or one object if ID is passed.
+      - DELETE: Deletes object what have the passed ID.
+                Or returns 404 page if no ID passed.
     """
     # Importing needed files
     from models.state import State
@@ -32,17 +34,32 @@ def stateEdit(state_id=None):
             for name in fullList.values():
                 entry = name.to_dict()
                 data.append(entry)
-            return (json.dumps(data, indent=2),
+            return (json.dumps(data, indent=2, sort_keys=True),
                     {"Content-Type": "application/json"})
         # When there's an ID
         seek = "State." + state_id
         # Search for the value
         try:
             found = fullList[seek].to_dict()
-            return (json.dumps(found, indent=2),
+            return (json.dumps(found, indent=2, sort_keys=True),
                     {"Content-Type": "application/json"})
         except KeyError:
             abort(404)
     
     # Using HTTP DELETE
     if req.method == "DELETE":
+        # No ID passed
+        if not state_id:
+            abort(404)
+        # ID passed
+        try:
+            seek = "State." + state_id
+            # If found object with such ID
+            storage.delete(fullList[seek])
+            storage.save()
+            emptData = {}
+            return (json.dumbs(emptData),
+                    {"Content-Type": "application/json"}, 200)
+        except KeyError:
+            # No object with such ID
+            abort(404)
