8eb3055cdb9a323056efc3a119a55c4b08885e82
MegaChie <workspace.mega@gmail.com>
2024-03-29 18:36:20 -0700

    404 page not found endpoint

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,7 +4,7 @@
 The first endpoint that returns the status of your API
 """
 from flask import Flask as fl
-from flask import jsonify as jsny
+import json
 from models import storage
 from api.v1.views import app_views
 
@@ -12,6 +12,15 @@ from api.v1.views import app_views
 app = fl(__name__)
 # Blueprint
 app.register_blueprint(app_views)
+
+
+
+@app.errorhandler(404)
+def pageNotFound(error):
+    """Handels the 404 page"""
+    badStat = {"error": "Not found"}
+    data = json.dumps(badStat, indent=2) + "\n"
+    return data, {"Content-Type": "application/json"}
 
 
 @app.teardown_appcontext
