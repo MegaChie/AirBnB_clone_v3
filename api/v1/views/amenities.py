7720a746605ecb73a209eaae9616d6fdd5dfc12a
mohamed-bo <medprgrm@gmail.com>
2024-03-31 18:38:46 +0100

    inchalah it works

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -26,7 +26,7 @@ def getAmenity(amenity_id):
 
 @app_views.route('/amenities/<amenity_id>', methods=['DELETE'],
                  strict_slashes=False)
-def deleteAmenity(amenity_id):
+def del_amenities(amenity_id):
     """deletes an amenity"""
     a = storage.get("Amenity", amenity_id)
     if not a:
@@ -37,7 +37,7 @@ def deleteAmenity(amenity_id):
 
 
 @app_views.route('/amenities', methods=['POST'], strict_slashes=False)
-def newAmenity():
+def post_amenity():
     """new amenity"""
     amenty = request.get_json()
     if not amenty:
@@ -54,7 +54,7 @@ def newAmenity():
 
 @app_views.route('/amenities/<amenity_id>', methods=['PUT'],
                  strict_slashes=False)
-def update(amenity_id):
+def put_amenity(amenity_id):
     """update amenity"""
     amenity = storage.get("Amenity", amenity_id)
     if not amenity:
