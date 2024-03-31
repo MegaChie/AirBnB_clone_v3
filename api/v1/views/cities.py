7d940dca6ce3df6160dcfb7d6d39d01564b100ce
MegaChie <workspace.mega@gmail.com>
2024-03-31 08:35:34 -0700

    HTTP POST method, GET undone

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -12,7 +12,7 @@ from models.city import City
 def cityAPI(state_id):
     """
     GET: Retrieves the list of all City objects if no ID is passed.
-        Or all the cities in a sertain state if an ID is passed.
+         Or all the cities in a sertain state if an ID is passed.
     """
     # All states and Cities
     fullState = storage.all(State)
@@ -27,7 +27,30 @@ def cityAPI(state_id):
             for city in found.fullCity:
                 entry = city.to_dict()
                 data.append(entry)
-                retrun (json.dumps(data, indent=2),
-                {"Content-Type": "application/json"})
+            retrun (json.dumps(data, indent=2),
+                    {"Content-Type": "application/json"})
         except KeyError:
             abort(404)
+
+    # Using HTTP POST
+    if req.method == "POST":
+        # Data is correct
+        if req.is_json:
+            new = req.get_json()
+        # Data not correct
+        else:
+            abort(400, "Not a JSON")
+        # Name given
+        if "name" in new:
+            seek = "State." + state_id
+            # No such ID
+            if seek not in fullState:
+                abort(404)
+            new.update({"state_id": state_id})
+            newCity = City(**new)
+            storage.new(newCity)
+            storage.save()
+            data = newCity.to_dict()
+            return jsny(data), 201
+        else:
+            abort(400, "Missing name")
