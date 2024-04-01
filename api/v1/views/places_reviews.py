f3aa860bf9c019ec0c65bd4eccb258ab47e30132
mohamed-bo <medprgrm@gmail.com>
2024-04-01 18:40:59 +0100

    inchalah it works

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -2,13 +2,15 @@
 """API endpoint for the reviews object"""
 from api.v1.views import app_views
 from flask import jsonify as jsny, make_response, abort, request as req
+import json
+from os import getenv
 from models import storage
 from models.place import Place
 from models.review import Review
 from models.user import User
 
 
-@app_views.route("/places/<place_id>/reviews", methods=["GET", "POST"],
+@app_views.route("/searchedPlaces/<place_id>/reviews", methods=["GET", "POST"],
                  strict_slashes=False)
 def reviewAPI(place_id):
     """
@@ -103,3 +105,59 @@ def putReview(review_id):
             setattr(review, k, value)
     storage.save()
     return make_response(jsny(review.to_dict()), 200)
+
+
+@app_views.route('/places_search', methods=['POST'], strict_slashes=False)
+def places_search():
+    """
+    get place depend
+    """
+    reques = req.get_json()
+    if reques is None:
+        abort(400, "Not a JSON")
+    if reques is None or (req.get('states') is None and
+                       req.get('cities') is None and
+                       req.get('amenities') is None
+                      ):
+        listPlaces = storage.all(Place)
+        return jsny([place.to_dict() for place in listPlaces.values()])
+    places = []
+    if reques.get('states'):
+        states = [storage.get("State", id) for id in reques.get('states')]
+        for state in states:
+            for city in state.cities:
+                for place in city.places:
+                    places.append(place)
+    if reques.get('cities'):
+        cities = [storage.get("City", id) for id in reques.get('cities')]
+        for c in cities:
+            for p in c.places:
+                if p not in places:
+                    places.append(place)
+    if not places:
+        places = storage.all(Place)
+        places = [p for p in places.values()]
+    if reques.get('amenities'):
+        listAmenity = [storage.get("Amenity", id) for id in reques.get('amenities')]
+        currentPort = getenv('HBNB_API_PORT')
+        port = 5000
+        if currentPort:
+            port = currentPort
+        loopUrl = "http://0.0.0.0:{}/api/v1/places/".format(port)
+        i = 0
+        breakPoint = len(places)
+        while i < breakPoint:
+            place = places[i]
+            url = loopUrl + '{}/amenities'
+            req = url.format(place.id)
+            res = req.get(req)
+            amnityInpage = json.loads(res.text)
+            amenities = [storage.get("Amenity", o['id']) for o in amnityInpage]
+            for amenity in listAmenity:
+                if amenity not in amenities:
+                    places.pop(i)
+                    breakPoint -= 1
+                    i -= 1
+                    break
+            i += 1
+    return jsny([place.to_dict() for place in places])
