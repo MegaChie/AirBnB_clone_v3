b2f729df4d409b3cf169e38a4ffef3ee524f4a4d
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-04-01 13:11:47 +0200

    Atempt to fix Amenity

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -1,87 +1,80 @@
 #!/usr/bin/python3
-"""Amenity View"""
+"""Routings for amenity-related API requests
+"""
+
 from api.v1.views import app_views
-from flask import jsonify as jsny, abort, request as req
+from flask import jsonify, abort, request
 from models import storage
 
 
-@app_views.route("/amenities", methods=["GET", "POST"], strict_slashes=False)
-@app_views.route("/amenities/<amenity_id>", methods=["GET", "DELETE", "PUT"],
+@app_views.route('/amenities', methods=['GET', 'POST'], strict_slashes=False)
+@app_views.route('/amenities/<amenity_id>', methods=['GET', 'DELETE', 'PUT'],
                  strict_slashes=False)
-def amenAPI(amenity_id=None):
-    """
-     - GET: Retrieves the list of all Amenity objects if no ID passed.
-            One object with ID passed.
-     - DELETE: Remove object if ID passed.
-               Return error code if ID is none or incorrect.
-     - POST: Add object if name is passed.
-             Returns error code if not passed or input is bad.
-     - PUT: Updates object if input is correct. Returns error code otherwise.
+def amenity_methods(amenity_id=None):
+    """Handle requests to API for amentities
     """
     from models.amenity import Amenity
-    fullList = storage.all(Amenity)
+    amenities = storage.all(Amenity)
 
-    # Using HTTP GET
-    if req.method == "Get":
-        # All is returned
-        if not amenity_id:
-            data = []
-            for name in fullList.values():
-                entry = name.to_dict()
-                data.append(entry)
-            return jsny(data)
-        # ID passed
-        seek = "Amenity." + amenity_id
-        try:
-            found = fullList[seek]
-            data = found.to_dict()
-            return jsny(data), 200
+    # GET REQUESTS
+    if request.method == 'GET':
+        if not amenity_id:  # if no id specified, return all
+            return jsonify([obj.to_dict() for obj in amenities.values()])
+
+        key = 'Amenity.' + amenity_id
+        try:  # if obj exists in dictionary, convert from obj -> dict -> json
+            return jsonify(amenities[key].to_dict())
         except KeyError:
+            abort(404)  # Amenity with amenity_id does not exist
+
+    # DELETE REQUESTS
+    elif request.method == 'DELETE':
+        try:
+            key = 'Amenity.' + amenity_id
+            storage.delete(amenities[key])
+            storage.save()
+            return jsonify({}), 200
+        except:
             abort(404)
 
-    # Using HTTP DELETE
-    if req.method == "DELETE":
-        try:
-            seek = "Amenity." + amenity_id
-            storage.delete(fullList[seek])
-            storage.save()
-            emptData = {}
-            return jsny(emptData), 200
-        except Exception:
-            abort(404)
-
-    # Using HTTP POST
-    if req.method == "POST":
-        if req.is_json:
-            new = req.get_json()
+    # POST REQUESTS
+    elif request.method == 'POST':
+        # convert JSON request to dict
+        if request.is_json:
+            body_request = request.get_json()
         else:
-            abort(400, "Not a JSON")
-        if "name" in new:
-            newAmen = Amenity(**new)
-            storage.new(newAmen)
-            storage.save()
-            data = newAmen.to_dict()
-            return jsny(data), 201
-        else:
-            abort(400, "Missing name")
+            abort(400, 'Not a JSON')
 
-    # Using HTTP PUT
-    if req.method == "PUT":
-        seek = "Amenity." + amenity_id
+        # instantiate, store, and return new Amenity object
+        if 'name' in body_request:
+            new_amenity = Amenity(**body_request)
+            storage.new(new_amenity)
+            storage.save()
+            return jsonify(new_amenity.to_dict()), 201
+        else:  # if request does not contain required attribute
+            abort(400, 'Missing name')
+
+    # PUT REQUESTS
+    elif request.method == 'PUT':
+        key = 'Amenity.' + amenity_id
         try:
-            toEdit = fullList[seek]
-            if req.is_json:
-                edit = req.get_json()
+            amenity = amenities[key]
+
+            # convert JSON request to dict
+            if request.is_json:
+                body_request = request.get_json()
             else:
-                abort(400, "Not a JSON")
-            for key, valu in edit.items():
-                if key not in ["id", "created_at", "updated_at"]:
-                    setattr(toEdit, key, valu)
+                abort(400, 'Not a JSON')
+
+            for key, val in body_request.items():
+                if key != 'id' and key != 'created_at' and key != 'updated_at':
+                    setattr(amenity, key, val)
+
             storage.save()
-            data = toEdit.to_dict()
-            return jsny(data), 200
+            return jsonify(amenity.to_dict()), 200
         except KeyError:
             abort(404)
 
+    # UNSUPPORTED REQUESTS
     else:
         abort(501)
